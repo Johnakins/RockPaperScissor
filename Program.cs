3454bcab9386f98f703f7eb3dfb3705d1b2ee865
Johnakins <johnolawole1@gmail.com>
2024-03-21 19:01:31 +0100

    Rock-paper-scissor game without dependency injection

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace RockPaperScissor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameManager gm = new GameManager();
+
+            do
+            {
+                RoundResult result = gm.Playground();
+
+                if (result == RoundResult.Player1Win)
+                {
+                    System.Console.WriteLine("Player 1 wins");
+                }
+                else if (result == RoundResult.Player2Win)
+                {
+                    System.Console.WriteLine("Player 2 wins");
+                }
+                else
+                {
+                    System.Console.WriteLine("It's a draw!");
+                }
+                System.Console.Write("Play again (Y/N)? ");
+            } while (Console.ReadLine().ToUpper() == "Y");
+        }
+    }
+}
